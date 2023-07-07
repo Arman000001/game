5e928de258df6654c24cec6c2fd965545b9a3cb8
Arman000001 <arman.nersisyan.d@tumo.org>
2023-07-07 19:52:55 +0400

    4rd commit

---
+++ animals.js
@@ -0,0 +1,71 @@
+const animals = [
+    {
+        "img": "img/skyur.png",
+        "name": "սկյուռ"
+
+    },
+    {
+        "img": "img/dzi.jpeg",
+        "name": "Ձի"
+
+    },
+    {
+        "img": "img/vagr.jpeg",
+        "name": "վագր"
+    },
+    {
+        "img": "img/kenguru.png",
+        "name": "կենգուրու"
+
+    },
+    {
+        "img": "img/pix.jpeg",
+        "name": "փիղ"
+
+    },
+    {
+        "img": "img/zebr.jpeg",
+        "name": "զեբրա"
+
+    },
+    {
+        "img": "img/kria.webp",
+        "name": "կրիա"
+
+    // },
+    // {
+    //     "img": "img/kapik.jpeg",
+    //     "name": "կապիկ"
+
+    },
+    {
+        "img": "img/axves.webp",
+        "name": "աղվես"
+
+    },
+    {
+        "img": "img/panda.jpeg",
+        "name": "պանդա"
+
+    },
+    {
+        "img": "img/vozni.webp",
+        "name": "ոզնի"
+
+    },
+    {
+        "img": "img/kov.webp",
+        "name": "կով"
+
+     },
+    {
+        "img": "img/begemot.webp",
+        "name": "բեգեմոտ"
+
+    },
+    {
+        "img": "img/pingvin.jpeg",
+        "name": "պինգվին"
+
+    }
+]
